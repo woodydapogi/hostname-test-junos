a10064c838e68879f5287ba1251374a50d77f12c
woodydapogi <21982601+woodydapogi@users.noreply.github.com>
2020-08-04 20:14:09 +0800

    Create hostname.py

---
+++ hostname.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#use ssh-key in production.
+try:
+    from ncclient import manager
+    from ncclient.xml_ import new_ele, sub_ele
+except ImportError as err:
+    print(err)
+
+#connect to routers.
+routers = [f'192.168.1.{n}' for n in range(99, 103)]
+loopback = [f'1.1.1{n}' for n in range(1, 3)]
+
+def conn(username, passwd, port):
+    for router, n in zip(routers, range(1, 5)):
+        with manager.connect(host= router,
+                username= username,
+                password= passwd,
+                port= port,
+                timeout= 10,
+                device_params= {'name': 'junos'},
+                hostkey_verify= False) as dev:
+
+            if dev:
+                print(f'{router} - connected.')
+
+                #configure hostname
+                hostname= new_ele('system')
+                sub_ele(hostname, 'host-name').text= f'router{n}'
+
+                #lock config
+                dev.lock()
+
+                #load hostname configuration
+                dev.load_configuration(config= hostname)
+
+                #validate configuration
+                dev.validate()
+
+                #commit confirm configuration
+                dev.commit(confirmed= True, timeout= '300')
+
+                #unlock configuration
+                dev.unlock()
+
+if __name__ == '__main__':
+        conn('root', 'Password123', '830')
